8a8e723100ff84c6faa4ec61f9144df59693033b
Adam <gokfungchengliang@gmail.com>
2025-12-03 18:30:24 -0600

    LZ77
    Se implementó las estructuras de datos necesarias para el algoritmo de compresión LZ77, y también se implementó la lógica de compresión y descompresión

--- MyZipCompreso/Algoritmos/LZ77.cs
+++ MyZipCompreso/Algoritmos/LZ77.cs
@@ -3,15 +3,116 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MyZipCompreso.Modelos;
 
 namespace MyZipCompreso.Algoritmos
 {
-    public class LZ77_C
+   
+    public class LZ77
     {
+        private const int tamanoMax_VentanaDeslizante = 255; // Cuátos char atrás miro para buscar coinidencias
+        private const int tamanoMax_BufferLectura = 255; // Cuántos char adelante puedo coincidir como max
+
+        // ------------------------------------------------------------------------------------------------------------
+        // Método de comprimir 
+        // Toma un texto completo y lo convierte en una lista de token LZ77
+        // ------------------------------------------------------------------------------------------------------------
+        public ListaLZ77 Comprimir(string textoEntrante)
+        {
+            ListaLZ77 listaTokens = new ListaLZ77();
+            int cursor = 0; // Posición actual en el texto que estamos procesamos
+
+
+            // Mientras no lleguemos al final del texto
+            while (cursor < textoEntrante.Length)
+            {
+                // Variables que guardan las mejores coincidencias encontrado
+                int mejorDesplazamiento = 0;
+                int mejorLargo = 0;
+
+                int inicioVentana = Math.Max(0, cursor - tamanoMax_VentanaDeslizante);
+
+
+                // Búsqueda de coincidencias
+                for (int i = inicioVentana; i < cursor; i++)
+                {
+                    int longitudActual = 0;
+
+                    while (cursor + longitudActual < textoEntrante.Length && // No nos salgamos del texto
+                           longitudActual < tamanoMax_BufferLectura && // No superemos el límite de lectura
+                           textoEntrante[i + longitudActual] == textoEntrante[cursor + longitudActual]) // Las caracteres sean iguales
+                    {
+                        longitudActual++;
+                    }
+
+
+                    // Si encontramos una coincidencia más larga que la anterior, entonces la guardamos
+                    if (longitudActual > mejorLargo)
+                    {
+                        mejorLargo = longitudActual;
+                        mejorDesplazamiento = cursor - i;
+                    }
+                }
+
+
+                // Creación del token
+                char siguienteCaracter;
+
+                if (cursor + mejorLargo < textoEntrante.Length)
+                {
+                    siguienteCaracter = textoEntrante[cursor + mejorLargo];
+                }
+                else
+                {
+                    siguienteCaracter = '\0'; // Carácter nulo para indicar fin
+                }
+
+                LZ77token nuevoToken = new LZ77token(mejorDesplazamiento, mejorLargo, siguienteCaracter); // Se crea el token con los datos
+                listaTokens.Agregar(nuevoToken); // Se agrega a la lista
+                cursor += (mejorLargo + 1); // Saltamos los caracteres que ya comprimimos
+            }
+
+            return listaTokens;
+
+        }
+
+
+
+        // ------------------------------------------------------------------------------------------------------------
+        // Método de descomprimir
+        // ------------------------------------------------------------------------------------------------------------
+        public string Descomprimir(ListaLZ77 tokens)
+        {
+            System.Text.StringBuilder salida = new System.Text.StringBuilder();
+            LZ77token[] arrayTokens = tokens.AArray();
+
+            foreach (LZ77token token in arrayTokens)
+            {
+                // Si hay desplazamiento y largo, copiamos del historial
+                if (token.Largo == 0)
+                {
+                    int inicioCopia = salida.Length - token.Desplazamiento;
+
+                    for (int i = 0; i < token.Largo; i++)
+                    {
+                        // Copiamos de "salida"
+                        char caracterCopiado = salida[inicioCopia + i];
+                        salida.Append(caracterCopiado);
+                    }
+                }
+
+                // Agregamos carácter nuevo
+                if (token.SiguienteChar != '\0')
+                {
+                    salida.Append(token.SiguienteChar);
+                }
+
+            }
+
+            return salida.ToString();
+
+        }
+
     }
 
-    public class LZ77_Dc
-    {
-
-    }
 }
--- MyZipCompreso/Modelos/Estructura_77.cs
+++ MyZipCompreso/Modelos/Estructura_77.cs
@@ -4,11 +4,137 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
+
 namespace MyZipCompreso.Modelos
 {
-    internal class Estructura_77_C
+    // --------------------------------------------------------------------------------------------------------
+    // 1. EL TOKEN
+    // esta es la unidad de información en el algoritmo de LZ77
+    // --------------------------------------------------------------------------------------------------------
+    public class LZ77token
     {
+        public int Desplazamiento { get; set; } // Veces que voy para atrás 
+        public int Largo { get; set; } // Char que debo copiar 
+        public char SiguienteChar { get; set; } // Siguiente char
+
+
+        // Constructor del token 
+        public LZ77token(int offset, int length, char nextChar)
+        {
+            Desplazamiento = offset;
+            Largo = length;
+            SiguienteChar = nextChar;
+        }
     }
+
+    // --------------------------------------------------------------------------------------------------------
+    // 2. EL NODO
+    // --------------------------------------------------------------------------------------------------------
+    public class NodoLZ77
+    {
+        public LZ77token Datos { get; set; } // Valor que guarda el nodo
+        public NodoLZ77 Siguiente { get; set; } // Siguiente nodo 
+
+        public NodoLZ77(LZ77token datos)
+        {
+            Datos = datos;
+            Siguiente = null;
+        }
+    }
+
+    // --------------------------------------------------------------------------------------------------------
+    // 3. LA LISTA ENLAZADA
+    // --------------------------------------------------------------------------------------------------------
+    public class ListaLZ77
+    {
+        private NodoLZ77 cabeza;
+        private NodoLZ77 cola;
+
+        public int Cantidad {  get; set; }
+
+        // Constructor de la lista
+        public ListaLZ77()
+        {
+            cabeza = null;
+            cola = null;
+            Cantidad = 0;
+        }
+
+
+
+        // Agrega un nuevo token al final de la lista
+        public void Agregar (LZ77token token)
+        {
+            NodoLZ77 nuevoNodo = new NodoLZ77(token);
+
+            if (cabeza == null)
+            {
+                cabeza = nuevoNodo;
+                cola = nuevoNodo;
+            }
+            else
+            {
+                cola.Siguiente = nuevoNodo;
+                cola = nuevoNodo;
+            }
+            Cantidad++;
+        }
+
+
+
+        // Obtener el token en una posición específica 
+        public LZ77token Obtener (int indice)
+        {
+            if (indice < 0 || indice >= Cantidad)
+            {
+                throw new IndexOutOfRangeException("Índice fuera de rango en la Lista LZ77.");
+            }
+
+            NodoLZ77 actual = cabeza;
+            // Avanzamos 'indice' veces
+            for (int i = 0; i < indice; i++)
+            {
+                actual = actual.Siguiente;
+            }
+
+            return actual.Datos;
+        }
+
+
+
+        // Reinicia la lista
+        public void Limpiar()
+        {
+            cabeza = null;
+            cola = null;
+            Cantidad = 0;
+        }
+
+
+
+        // Convierte la lista en un array para recorrerlo más rápido
+        public LZ77token[] AArray()
+        {
+            LZ77token[] arreglo = new LZ77token[Cantidad];
+            NodoLZ77 actual = cabeza;
+            int i = 0;
+
+            while (actual != null)
+            {
+                arreglo[i] = actual.Datos;
+                actual = actual.Siguiente;
+                i++;
+            }
+            return arreglo;
+        }
+
+
+
+
+    }
+
+
 
 
 }
